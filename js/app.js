c94a655695a5c45143e6b855f1bb4f988ff31ee5
marcmedhat6211 <marcmedhat6211@gmail.com>
2025-06-26 12:19:21 +0300

    few adjustments before the deployment

--- js/app.js
+++ js/app.js
@@ -2,29 +2,28 @@ $(document).ready(function () {
   // services slider
   const servicesSwiper = new Swiper(".services-section .swiper", {
     loop: true,
-    // centeredSlides: true,
-    slidesPerView: 3,
+    centeredSlides: true,
+    slidesPerView: 1.5,
     spaceBetween: 30,
     breakpoints: {
-      0: {
-        slidesPerView: 1.5,
-        centeredSlides: true,
-      },
-      768: {
-        slidesPerView: 1.5,
-        centeredSlides: true,
-      },
-      1024: {
-        slidesPerView: 1.5,
-        centeredSlides: true,
+      992: {
+        slidesPerView: 2.9,
+        centeredSlides: false,
       },
     },
   });
 
   const partnersSwiper = new Swiper(".partners-section .swiper", {
     loop: true,
-    slidesPerView: 5,
+    slidesPerView: 1.9,
+    centeredSlides: true,
     spaceBetween: 20,
+    breakpoints: {
+      992: {
+        slidesPerView: 5,
+        centeredSlides: false,
+      },
+    },
   });
 
   $("#mobile_menu_btn").on("click", function () {
